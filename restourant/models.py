c0b5f07131c6240dea43b8ae2f698e50e1018652
Your Name <you@example.com>
2017-07-26 14:36:26 +0300

    menu

--- restourant/models.py
+++ restourant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Dish(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.CharField(max_length=200)
+	price = models.FloatField()
+
+	def __str__(self):
+		return self.name
+
+	def toJSON(self):
+		return {"dish": {"id": self.id, "name": self.name, "description": self.description, "price": self.price}}
--- restourant/views.py
+++ restourant/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import auth
 from django.middleware import csrf
+from restourant.models import Dish
 import json
 
 def index(request):
@@ -19,3 +20,11 @@ def login(request):
 			return HttpResponse('{"result": false, "error": "There is no such user"}', content_type='application/json')
 	elif request.method == "GET":
 		return HttpResponse(csrf.get_token(request))
+
+def menu(request):
+	if request.user.is_authenticated:
+		dishes = list(Dish.objects.all())
+		jsonStr = json.dumps([d.toJSON() for d in dishes], ensure_ascii=False)
+		return HttpResponse(jsonStr, content_type='application/json')
+
+	return HttpResponse("Your are not authenticated")
